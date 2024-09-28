d392d1b46c4cb7e838d5c4693517065c66fd67a1
M-Vince <165883810+M-Vince@users.noreply.github.com>
2024-09-28 13:32:03 +0800

    Update To-Do App: Added basic features

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,3 +1,48 @@
 const input = document.querySelector('.create-item-input');
 const addBtn = document.querySelector('.create-item-btn');
 const taskList = document.querySelector('.task-list');
+
+
+addBtn.addEventListener('click', () => {
+
+    if (input.value) {
+        console.log(input.value);
+
+        const addTask = document.createElement('li');
+        const title = document.createElement('h3');
+        const delBtn = document.createElement('button');
+
+        addTask.classList.add('task-items');
+        delBtn.classList.add('task-item-btn');
+        title.innerText = input.value;
+        delBtn.innerText = `Delete`;
+        addTask.append(title);
+        addTask.append(delBtn);
+
+        taskList.append(addTask);
+    }
+})
+
+
+taskList.addEventListener('click', (e) => {
+    if(e.target.nodeName === 'BUTTON') {
+        // e.target.remove();
+        console.log(e);
+        // e.target.parentNode.remove();
+
+        if(e.target.innerText === 'Delete') {
+            const target = e.target.parentNode;
+            target.parentNode.remove();
+        }
+
+
+    }
+})
+
+
+
+
+{/* <li class="task-items"> 
+    <h3>Record New Video Content</h3>
+    <button class="task-item-btn"> Delete </button> 
+</li> */}
